95aae487595614a89dfc5927ebe3747dd7f45250
Adam <adam.mehdaouijorge@gmail.com>
2023-05-21 16:11:32 +0200

    display timeboard

--- src/fr/uge/patchwork/view/View.java
+++ src/fr/uge/patchwork/view/View.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 
 import java.awt.*;
 import java.nio.file.Path;
+import java.sql.Time;
 import java.util.Arrays;
 
 import fr.uge.patchwork.model.Cell;
@@ -14,67 +15,66 @@ import fr.umlv.zen5.*;
 import javax.imageio.ImageIO;
 
 public class View {
+
     public static void initView(TimeBoard timeBoard){
         Application.run(Color.BLACK, context -> {
+            BufferedImage filledSquare = fileToImage("data/Board/filledSquare.png", 30, 30);
+            BufferedImage outlineSquare = fileToImage("data/Board/outlineSquare.png", 30, 30);
+            BufferedImage outlineSquareTB = fileToImage("data/Board/outlineSquare.png", 60, 60);
+            BufferedImage button = fileToImage("data/Board/button.png", 30, 30);
+            BufferedImage patch = fileToImage("data/Board/patch.png", 30, 30);
 
-            BufferedImage filledSquare = fileToImage("data/Default (64px)/d6.png", 30, 30);
-            BufferedImage outlineSquare = fileToImage("data/Default (64px)/d6_outline.png", 30, 30);
-
-            BufferedImage player1 = fileToImage("data/Player/player1.png", 45, 45);
+            BufferedImage player1 = fileToImage("data/Player/player1.png", 80, 80);
             BufferedImage player2 = fileToImage("data/Player/player2.png", 45, 45);
-
-            BufferedImage button = fileToImage("data/Default (64px)/d2.png", 40, 40);
-            BufferedImage patch = fileToImage("data/Default (64px)/card_add.png", 40, 40);
-
-            Cell[][] tb = timeBoard.formSpiralMatrix();
+            BufferedImage tagPlayer1 = fileToImage("data/Player/tagPlayer1.png", 45, 45);
+            BufferedImage tagPlayer2 = fileToImage("data/Player/tagPlayer2.png", 45, 45);
 
             context.renderFrame(graphics2D -> {
                 graphics2D.setColor(Color.BLACK);
                 graphics2D.fillRect(0, 0, (int) context.getScreenInfo().getWidth(), (int) context.getScreenInfo().getHeight());
 
-                for (int i = 0; i < 9; i++) {
-                    for (int j = 0; j < 9; j++){
-                        graphics2D.drawImage(outlineSquare, j * 32 + 20, i * 32 + 20, null);
-                        graphics2D.drawImage(outlineSquare, ((int) context.getScreenInfo().getWidth() - (j * 32 + 60)), i * 32 + 20, null);
+                /* Drawing the tag of players */
+                graphics2D.drawImage(tagPlayer1, 0, 0, null);
+                graphics2D.drawImage(tagPlayer2, (int) context.getScreenInfo().getWidth() - 65, 0, null);
+
+                /* Drawing the board of player 1 and player 2 */
+                int i;
+                int j;
+                for (i = 0; i < 9; i++) {
+                    for (j = 0; j < 9; j++) {
+                        /* Player 1 board */
+                        graphics2D.drawImage(outlineSquare, 65 + j * 32, i * 32, null);
+                        /* Player 2 board */
+                        graphics2D.drawImage(outlineSquare, (int) (context.getScreenInfo().getWidth() / 1.3 + j * 32),
+                                i * 32, null);
                     }
                 }
 
-                // double for du dessin de la spirale: ne marche pas car formSpiralMatrix() creer 64 case et non 54
-//                for (int i = 0; i < 8; i++) {
-//                    for (int j = 0; j < 8; j++){
-//                        graphics2D.drawImage(outlineSquare, (((int) context.getScreenInfo().getWidth()/3) + (j * 32 + 20)), i * 32 + 20, null);
-//
-//                        if (tb[i][j].patch()){
-//                            graphics2D.drawImage(patch, (((int) context.getScreenInfo().getWidth()/3) + (j * 32 + 20)), i * 32 + 20, null);
-//                        }
-//                        if (tb[i][j].button()){
-//                            graphics2D.drawImage(button, (((int) context.getScreenInfo().getWidth()/3) + (j * 32 + 20)), i * 32 + 20, null);
-//                        }
-//                        if (tb[i][j].player1() != null){
-//                            graphics2D.drawImage(player1, (((int) context.getScreenInfo().getWidth()/3) + (j * 32 + 20)), i * 32 + 20, null);
-//                        }
-//                        if (tb[i][j].player2() != null){
-//                            graphics2D.drawImage(player2, (((int) context.getScreenInfo().getWidth()/3) + (j * 32 + 20)), i * 32 + 20, null);
-//                        }
-//                    }
-//                }
+                int xDrawSquare;
+                int yDrawSquare;
+                int cellInd;
+                /* Drawing the time board */
+                for (i = 0; i < 6; i++) {
+                    for (j = 0; j < 8; j++) {
+                        xDrawSquare = (int) ((context.getScreenInfo().getWidth() / 2) + i * 80 - 220);
+                        yDrawSquare = (int) ((context.getScreenInfo().getHeight() / 2) + j * 60 - 120);
+                        cellInd = j * 6 + i;
 
-                for (int j = 0; j < 54; j++){
-                        graphics2D.drawImage(outlineSquare, (j * 32 + 20), (int) context.getScreenInfo().getHeight()/2, null);
+                        graphics2D.drawImage(outlineSquareTB, xDrawSquare, yDrawSquare, null);
 
-                        if (timeBoard.cells.get(j).patch()){
-                            graphics2D.drawImage(patch, (j * 32 + 20), (int) context.getScreenInfo().getHeight()/2, null);
+                        if (timeBoard.cells.get(cellInd).button()) {
+                            graphics2D.drawImage(button, xDrawSquare + 35, yDrawSquare + 15, null);
                         }
-                        if (timeBoard.cells.get(j).button()){
-                            graphics2D.drawImage(button, (j * 32 + 20), (int) context.getScreenInfo().getHeight()/2, null);
+                        if (timeBoard.cells.get(cellInd).patch()) {
+                            graphics2D.drawImage(patch, xDrawSquare + 35, yDrawSquare + 15, null);
                         }
-                        if (timeBoard.cells.get(j).player1() != null){
-                            graphics2D.drawImage(player1, (j * 32 + 20), (int) context.getScreenInfo().getHeight()/2, null);
-                        }
-                        if (timeBoard.cells.get(j).player2() != null){
-                            graphics2D.drawImage(player2, (j * 32 + 20), (int) context.getScreenInfo().getHeight()/2, null);
-                        }
+
+//                        if (timeBoard.cells.get(cellInd).player1() != null) {
+//                            graphics2D.drawImage(player1, 800, 800, null);
+//                        }
+
                     }
+                }
             });
         });
     }
@@ -92,8 +92,7 @@ public class View {
 
     public static BufferedImage scale(BufferedImage src, int w, int h)
     {
-        BufferedImage img =
-                new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
+        BufferedImage img = new BufferedImage(w, h, BufferedImage.TYPE_INT_RGB);
         int x, y;
         int ww = src.getWidth();
         int hh = src.getHeight();
